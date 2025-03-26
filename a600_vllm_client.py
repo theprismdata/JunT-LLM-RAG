677ae34d23c8d1a9b288dbcbb465bcd538e2e761
HJSHIN\theprismdata <theprismdata@gmail.com>
2025-03-26 20:01:21 +0900

    prompt

--- a600_vllm_client.py
+++ a600_vllm_client.py
@@ -6,20 +6,28 @@ openai_api_base = "http://192.168.205.174:8889/v1"
 """
 meta-llama/Llama-3.2-3B-Instruct
 python -m vllm.entrypoints.openai.api_server --model meta-llama/Llama-3.2-3B-Instruct --host 0.0.0.0 --port 8889 --max-model-len=256
-"""
 
-"""
 yanolja/EEVE-Korean-10.8B-v1.0
 python -m vllm.entrypoints.openai.api_server --model yanolja/EEVE-Korean-10.8B-v1.0 --host 0.0.0.0 --port 8889 --max-model-len=256
+
+yanolja/EEVE-Korean-Instruct-10.8B-v1.0
+python -m vllm.entrypoints.openai.api_server --model yanolja/EEVE-Korean-Instruct-10.8B-v1.0 --host 0.0.0.0 --port 8889 --max-model-len=256
 """
 model_path = "yanolja"
-model_code = "EEVE-Korean-10.8B-v1.0"
+model_code = "EEVE-Korean-Instruct-10.8B-v1.0"
 
 client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
 )
 
-result = client.completions.create(model = f'{model_path}/{model_code}',
-                          prompt = "강남의 맛집을 찾아줘")
-print(result.choices[0].text)
+result = client.chat.completions.create(
+    model="yanolja/EEVE-Korean-Instruct-10.8B-v1.0",
+    messages=[
+        {
+            "role": "user",
+            "content": "강남은 어디에 잇어?"
+        }
+    ]
+)
+print(result.choices[0].message.content)
